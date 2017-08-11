9d6be6de2629a2ac212ba70f0768432af2e4da0d
Prajwol <imaginepraz8@gmail.com>
2017-08-10 19:30:53 -0500

    Third Commit Added Css,MasterPage

--- PioneerTech.WebApp/UI/PioneerTechMasterPage.Master.designer.cs
+++ PioneerTech.WebApp/UI/PioneerTechMasterPage.Master.designer.cs
@@ -13,22 +13,13 @@ namespace PioneerTech.WebApp.UI {
     public partial class PioneerTechMasterPages {
         
         /// <summary>
-        /// head control.
+        /// MainMasterPage control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
-        
-        /// <summary>
-        /// form1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        protected global::System.Web.UI.HtmlControls.HtmlForm MainMasterPage;
         
         /// <summary>
         /// ContentPlaceHolder1 control.
